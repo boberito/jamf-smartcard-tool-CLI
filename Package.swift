41792ba9c69df62798c60b4a7831de6b36a352f6
Bob Gendler <boberito@mac.com>
2023-04-19 10:09:32 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "SCEnforcement-CLI",
-            dependencies: []),
+            dependencies: [.product(name: "SwiftWin32", package: "swift-win32"),],path: "Sources),
         .testTarget(
             name: "SCEnforcement-CLITests",
             dependencies: ["SCEnforcement-CLI"]),
